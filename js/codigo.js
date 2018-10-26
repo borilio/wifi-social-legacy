657a2291a84ea3037dd9c9f15ddd2aa85a68d1a4
borilio <exprimetuandroid@gmail.com>
2018-10-26 15:35:40 +0200

    Versión casi casi final del multilenguaje. Quedan pulir algunos flecos.

--- js/codigo.js
+++ js/codigo.js
@@ -2,6 +2,12 @@
 Autor ExprimeAndroid(c) by Gobo Technologies
 */
 
+
+//TODO: Mover el formulario de user y pass debajo del botón y texto de condiciones de uso
+//TODO: Y añadir un párrafo diciendo, AHORA puede introducir sus credenciales y pulsar entrar...
+//TODO: Grabar un video y ponerlo en la ayuda (o capturas, o gif, si es muy grande)
+
+
 angular
 	.module("miModulo", ['ngMaterial', 'ngMessages'])
 	.controller("miControlador", ["$scope", "$mdDialog", "$mdToast", "$http" , mainController])
@@ -14,8 +20,9 @@ function mainController($scope, $mdDialog, $mdToast, $http) {
 	//Función que muestra el cuadro de dialogo para aceptar las condiciones
 	vm.mostrarCondiciones = function (ev) {
 			$mdDialog.show({
-					controller: DialogController,
-					templateUrl: 'condiciones.tmpl.html',
+					controller: vm.DialogController,
+                    locals : {m : vm.m},
+					templateUrl: 'templates/cond-' + vm.idioma + '.tmpl.html',
 					parent: angular.element(document.body),
 					targetEvent: ev,
 					clickOutsideToClose: false,
@@ -24,21 +31,22 @@ function mainController($scope, $mdDialog, $mdToast, $http) {
 				.then(function (answer) { //Respondió o true o false
 					vm.condiciones = answer;
 					if (answer){
-						$scope.status = "Has aceptado las condiciones de uso";
+						$scope.status = vm.m.condAcepto;
 					} else{
-						$scope.status = "No podrás navegar mientras no aceptes las condiciones";
+						$scope.status = vm.m.condNoAcepto;
                     }
 				}, function () {  //Canceló la ventana sin aceptar o declinar
 					vm.condiciones = false;
-					$scope.status = "Has cerrado la ventana sin aceptar o rechazar las condiciones. No podrás navegar mientras no sean aceptadas.";
+					$scope.status = vm.m.condElse;
 				});
 	};
 
     //Función que muestra el cuadro de dialogo de ayuda
 	vm.mostrarAyuda = function (ev) {
 			$mdDialog.show({
-					controller: DialogController,
-					templateUrl: 'ayuda.tmpl.html',
+					controller: vm.DialogController,
+                    locals : {m : vm.m},
+					templateUrl: 'templates/ayuda.tmpl.html',
 					parent: angular.element(document.body),
 					targetEvent: ev,
 					clickOutsideToClose: true,
@@ -56,8 +64,9 @@ function mainController($scope, $mdDialog, $mdToast, $http) {
     //Función que muestra el cuadro de dialogo para aceptar las condiciones
 	vm.mostrarSelectorIdioma = function (ev) {
 			$mdDialog.show({
-					controller: DialogController,
-					templateUrl: 'multilenguaje.tmpl.html',
+					controller: vm.DialogController,
+					locals : {m : vm.m},
+                    templateUrl: 'templates/multi.tmpl.html',
 					parent: angular.element(document.body),
 					targetEvent: ev,
 					clickOutsideToClose: true,
@@ -68,16 +77,19 @@ function mainController($scope, $mdDialog, $mdToast, $http) {
 				    //Rellenamos el objeto m con los atributos del JSON seleccionado
                     vm.cargarIdioma(vm.idioma);
                     //El toast lo mostramos en cargarIdioma(), ya que si no al ser asincrono, es posible que
-                    //cuando termine cargarIdioma() todavía vm.m.change valga todavía lo anterior y no salía bien el mensaje
+                    //cuando termine cargarIdioma() todavía vm.m.langChange valga todavía lo anterior y no salía bien el mensaje
 
                 }, function () {  //Canceló la ventana sin aceptar o declinar
 				    //No hacemos nada, lo dejamos todo tal y como está...
 				});
 	};
-	
+
 	//Función que muestra u oculta el cuadro de diálogo
-	function DialogController($scope, $mdDialog) {
-		$scope.hide = function () {
+	vm.DialogController = function ($scope, $mdDialog, m) {
+        //Pasamos los mensajes al dialogo poniéndolos en el $scope
+        $scope.m = m;
+
+        $scope.hide = function () {
 			$mdDialog.hide();
 		};
 
@@ -88,8 +100,7 @@ function mainController($scope, $mdDialog, $mdToast, $http) {
 		$scope.answer = function (answer) {
 			$mdDialog.hide(answer);
 		};
-	}
-
+	};
 
 	//Función que borra todos los valores y los resetea
 	vm.restart = function () {
@@ -99,8 +110,8 @@ function mainController($scope, $mdDialog, $mdToast, $http) {
 		vm.user="";
 		vm.pass="";
         vm.idioma = "es";
-        vm.idiomaLargo = "Español";
-	};
+        vm.cargarIdioma(vm.idioma);
+    };
 
 
     /**
@@ -134,7 +145,8 @@ function mainController($scope, $mdDialog, $mdToast, $http) {
     vm.cargarIdioma = function (codigo) {
         var exito = function(resp) {
             vm.m = resp.data;
-            vm.muestraToast(vm.m.change);
+            //Una vez cargados los datos del JSON, mostramos el toast
+            vm.muestraToast(vm.m.langChange);
 		};
 
 		var error = function(error) {
@@ -147,13 +159,13 @@ function mainController($scope, $mdDialog, $mdToast, $http) {
     };
 
     //NOTE: Una vez definido, aquí empieza la ejecución del programa
-
+    //FIXME: Arreglar ésto, creando un objeto JS tipo var app ={ atributos:... , métodos: ... }
     /******************************************************************************************/
     /********        ¡¡¡¡ Aquí empieza todo  !!!!   *******************************************/
     /******************************************************************************************/
 
     //Configuración básica
-    vm.empresa = "WIFI Gratis";
+    vm.empresa = "Nombre empresa";
     vm.copyright = "GoBo Technologies";
     vm.descripcion = vm.empresa;
     vm.numeroVersion = "2.0";
@@ -163,7 +175,6 @@ function mainController($scope, $mdDialog, $mdToast, $http) {
     vm.idioma = "es";
     vm.m = {};
     vm.cargarIdioma(vm.idioma);  //Y rellenamos el objeto m con los textos para el idioma seleccionado
-
 
     //Valores por defecto...
     vm.user="";                 //Usuario y contraseña por defecto que sale en el form
