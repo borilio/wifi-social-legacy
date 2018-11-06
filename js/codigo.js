643a91ee749cfb9e0e738895cc76ed59811510b7
borilio <exprimetuandroid@gmail.com>
2018-11-06 17:24:04 +0100

    Finalizando rama multilenguaje

--- js/codigo.js
+++ js/codigo.js
@@ -3,7 +3,6 @@ Autor ExprimeAndroid(c) by Gobo Technologies
 */
 
 
-//TODO: Y añadir un párrafo diciendo, AHORA puede introducir sus credenciales y pulsar entrar...
 //TODO: Grabar un video y ponerlo en la ayuda (o capturas, o gif, si es muy grande)
 
 
